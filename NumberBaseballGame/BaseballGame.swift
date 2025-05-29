79ae8616caaf94d58fb78cd107ad50bb526873ad
Pjh01 <estelle0102@naver.com>
2025-05-29 14:37:45 +0900

    Feat: Lv6

--- NumberBaseballGame/BaseballGame.swift
+++ NumberBaseballGame/BaseballGame.swift
@@ -8,48 +8,52 @@ class BaseballGame {
         while true {
             print("숫자를 입력하세요")
             
-            guard let userNumber = readLine(), let _ = Int(userNumber), checkInputNumber(userNumber) else {
+            guard let userNumber = readLine(), let _ = Int(userNumber) else {
                 print("올바르지 않은 입력값입니다\n")
                 continue
             }
             
+            let (isOk, userNumberArray) = checkInputNumber(userNumber)
+            guard isOk else {
+                print("서로 다른 숫자로 이루어진 3자리 수를 입력해주세요\n")
+                continue
+            }
+            
             trialCount += 1
-            if checkAnswer(userNumber: userNumber, answer: answer) {
+            if checkAnswer(userNumber: userNumberArray, answer: answer) {
                 break
             }
         }
         return trialCount
     }
     
-    func makeAnswer() -> Int {
+    func makeAnswer() -> [Int] {
         let firstNumber = Int.random(in: 1...9)
         let remainNumbers = Array(0...9).filter { $0 != firstNumber }.shuffled().prefix(2)
-        let answer = firstNumber * 100 + remainNumbers[0] * 10 + remainNumbers[1]
         
-        return answer
+        return [firstNumber] + Array(remainNumbers)
     }
     
-    func checkInputNumber(_ number: String) -> Bool {
+    func checkInputNumber(_ number: String) -> (isOk: Bool, userNumberArray: [Int]) {
         let numberArray = number.compactMap { $0.wholeNumberValue }
+        let isOk = numberArray.count == 3 && Set(numberArray).count == 3 && numberArray[0] != 0
         
-        return numberArray.count == 3 && Set(numberArray).count == 3 && numberArray[0] != 0
+        return (isOk, numberArray)
     }
     
-    func checkAnswer(userNumber: String, answer: Int) -> Bool {
+    func checkAnswer(userNumber: [Int], answer: [Int]) -> Bool {
         var strikeCount = 0
         var ballCount = 0
-        let userNumberArray = userNumber.compactMap { $0.wholeNumberValue }
-        let answerArray = String(answer).compactMap { $0.wholeNumberValue }
         
-        if userNumberArray == answerArray {
+        if userNumber == answer {
             print("정답입니다!\n")
             return true
         }
         
         for i in 0..<3 {
-            if userNumberArray[i] == answerArray[i] {
+            if userNumber[i] == answer[i] {
                 strikeCount += 1
-            } else if answerArray.contains(userNumberArray[i]) {
+            } else if answer.contains(userNumber[i]) {
                 ballCount += 1
             }
         }
--- NumberBaseballGame/GameManagement.swift
+++ NumberBaseballGame/GameManagement.swift
@@ -1,9 +1,10 @@
 class GameManagement {
     let baseballGame = BaseballGame()
     let gameRecord = GameRecord()
+    var isEnd = false
     
     func startGame() {
-        while true {
+        while !isEnd {
             print("환영합니다! 원하시는 번호를 입력해주세요\n1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기")
             
             guard let userInput = readLine() else {
@@ -17,8 +18,9 @@ class GameManagement {
                 gameRecord.addRecord(record)
             case "2":
                 gameRecord.printRecords()
-            //case "3":
-                //
+            case "3":
+                print("< 숫자 야구 게임을 종료합니다 >")
+                isEnd = true
             default:
                 print("올바른 숫자를 입력해주세요!\n")
             }
